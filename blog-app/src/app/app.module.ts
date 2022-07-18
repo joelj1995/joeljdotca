5ba71dbc1767c67fe7597ee768eb6e90ab55b2ca
Joel Johnston <colter13@gmail.com>
2022-07-17 18:56:45 -0600

    Build out the landing page

--- blog-app/src/app/app.module.ts
+++ blog-app/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { BlogRollComponent } from './blog-roll/blog-roll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LandingComponent,
+    BlogRollComponent
   ],
   imports: [
     BrowserModule,
---
+++ blog-app/src/app/blog-roll/blog-roll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-roll',
+  templateUrl: './blog-roll.component.html',
+  styleUrls: ['./blog-roll.component.scss']
+})
+export class BlogRollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
