e10df01a596982064fd46d1c58a19b208feccfd1
Joel Johnston <colter13@gmail.com>
2022-07-24 21:34:46 -0600

    Mount and unmount the file share as part of deployment

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -13,6 +13,9 @@ cp ./infrastructure/joeljca.htaccess ./joeljca/.htaccess
 mkdir joeljca/wordpress
 cp -r ./wordpress/ ./joeljca
 
+echo "[DEPLOY] Unmounting the file share"
+ssh service@$TARGET_HOST "sudo umount -f -l /srv/www/joeljca/wordpress/wp-content/uploads"
+
 echo '[DEPLOY] Removing site files on server'
 ssh service@$TARGET_HOST "sudo rm -rf /srv/www/joeljca"
 
@@ -20,6 +23,12 @@ echo '[DEPLOY] Syncing site files to server'
 rsync -az ./joeljca service@$TARGET_HOST:/srv/www
 rsync --rsync-path="sudo rsync" ./infrastructure/*.conf service@$TARGET_HOST:/etc/apache2/sites-enabled
 
+echo '[DEPLOY] Mounting the file share'
+# TODO: inject the name of the storage account so this is environment agnostic
+ssh service@$TARGET_HOST "sudo mkdir -p /srv/www/joeljca/wordpress/wp-content/uploads"
+ssh service@$TARGET_HOST "sudo mount -t nfs joeljca.file.core.windows.net:/joeljca/wp-uploads /srv/www/joeljca/wordpress/wp-content/uploads -o vers=4,minorversion=1,sec=sys"
+
+echo '[DEPLOY] Updating owner of /srv/www to www-data'
 ssh service@$TARGET_HOST "sudo chown -R www-data: /srv/www"
 
 echo '[DEPLOY] Deployment succeeded'
