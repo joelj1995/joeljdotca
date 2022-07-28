5875709d568394c9e21e31521bab8c5f08658724
Joel Johnston <colter13@gmail.com>
2022-07-27 21:16:01 -0600

    Make deployment generic for test environment

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -4,29 +4,35 @@ set -e
 
 echo "[DEPLOY] Deployment started against target host $1"
 TARGET_HOST=$1
+APACHE_SITE_NAME=$2
+WP_SITE_URL=$3
 
 echo '[DEPLOY] Staging site files locally'
-rm -rf joeljca
-mkdir joeljca
-cp ./blog-app/dist/blog-app/* ./joeljca
-cp ./infrastructure/joeljca.htaccess ./joeljca/.htaccess
-mkdir joeljca/wordpress
-cp -r ./wordpress/ ./joeljca
+rm -rf $APACHE_SITE_NAME
+mkdir $APACHE_SITE_NAME
+cp ./blog-app/dist/blog-app/* ./$APACHE_SITE_NAME
+cp ./infrastructure/joeljca.htaccess ./$APACHE_SITE_NAME/.htaccess
+mkdir $APACHE_SITE_NAME/wordpress
+cp -r ./wordpress/ ./$APACHE_SITE_NAME
 
 echo "[DEPLOY] Unmounting the file share"
-ssh service@$TARGET_HOST "sudo umount -f -l /srv/www/joeljca/wordpress/wp-content/uploads"
+ssh service@$TARGET_HOST "sudo umount -f -l /srv/www/$APACHE_SITE_NAME/wordpress/wp-content/uploads" || echo "Could not unmount share"
 
 echo '[DEPLOY] Removing site files on server'
-ssh service@$TARGET_HOST "sudo rm -rf /srv/www/joeljca"
+ssh service@$TARGET_HOST "sudo rm -rf /srv/www/$APACHE_SITE_NAME"
 
 echo '[DEPLOY] Syncing site files to server'
-rsync -az ./joeljca service@$TARGET_HOST:/srv/www
-rsync --rsync-path="sudo rsync" ./infrastructure/*.conf service@$TARGET_HOST:/etc/apache2/sites-enabled
+cp ./infrastructure/joeljca-ssl.conf ./infrastructure/$APACHE_SITE_NAME.conf
+sed -i "s/{{TARGET_HOST}}/$TARGET_HOST/" ./infrastructure/$APACHE_SITE_NAME.conf
+sed -i "s/{{APACHE_SITE_NAME}}/$APACHE_SITE_NAME/" ./infrastructure/$APACHE_SITE_NAME.conf
+sed -i "s/{{WP_SITE_URL}}/$WP_SITE_URL/" ./infrastructure/$APACHE_SITE_NAME.conf
+rsync -az ./$APACHE_SITE_NAME service@$TARGET_HOST:/srv/www
+rsync --rsync-path="sudo rsync" ./infrastructure/$APACHE_SITE_NAME.conf service@$TARGET_HOST:/etc/apache2/sites-enabled
 
 echo '[DEPLOY] Mounting the file share'
 # TODO: inject the name of the storage account so this is environment agnostic
-ssh service@$TARGET_HOST "sudo mkdir -p /srv/www/joeljca/wordpress/wp-content/uploads"
-ssh service@$TARGET_HOST "sudo mount -t nfs joeljca.file.core.windows.net:/joeljca/wp-uploads /srv/www/joeljca/wordpress/wp-content/uploads -o vers=4,minorversion=1,sec=sys"
+ssh service@$TARGET_HOST "sudo mkdir -p /srv/www/$APACHE_SITE_NAME/wordpress/wp-content/uploads"
+ssh service@$TARGET_HOST "sudo mount -t nfs joeljca.file.core.windows.net:/joeljca/wp-uploads /srv/www/$APACHE_SITE_NAME/wordpress/wp-content/uploads -o vers=4,minorversion=1,sec=sys"
 
 echo '[DEPLOY] Updating owner of /srv/www to www-data'
 ssh service@$TARGET_HOST "sudo chown -R www-data: /srv/www"
--- infrastructure/transform-wp-config.sh
+++ infrastructure/transform-wp-config.sh
@@ -15,8 +15,8 @@ cp ./wordpress/wp-config.php.template ./wordpress/wp-config.php
 sed -i "s/{{WP_DB_HOST}}/$WP_DB_HOST/" ./wordpress/wp-config.php
 sed -i "s/{{WP_DB_PASSWORD}}/$WP_DB_PASSWORD/" ./wordpress/wp-config.php
 sed -i "s/{{WP_CONFIG_KEYS}}/$(echo $WP_CONFIG_KEYS | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" ./wordpress/wp-config.php
-sed -i "s/{{WP_SITE_URL}}/$WP_DB_PASSWORD/" ./wordpress/wp-config.php
-sed -i "s/{{WP_DB_USER}}/$WP_DB_PASSWORD/" ./wordpress/wp-config.php
-sed -i "s/{{WP_DB_NAME}}/$WP_DB_PASSWORD/" ./wordpress/wp-config.php
+sed -i "s/{{WP_SITE_URL}}/$WP_SITE_URL/" ./wordpress/wp-config.php
+sed -i "s/{{WP_DB_USER}}/$WP_DB_USER/" ./wordpress/wp-config.php
+sed -i "s/{{WP_DB_NAME}}/$WP_DB_NAME/" ./wordpress/wp-config.php
 
-echo '[WP_CONFIG] Config transformation succeeded
+echo '[WP_CONFIG] Config transformation succeeded'
