c7616053365a2fd421a44eb8955079f0fe373f0b
Joel Johnston <colter13@gmail.com>
2022-07-27 21:54:04 -0600

    Use different NFS dir for each site instance

--- infrastructure/transform-wp-config.sh
+++ infrastructure/transform-wp-config.sh
@@ -9,6 +9,7 @@ WP_CONFIG_KEYS=$3
 WP_SITE_URL=$4
 WP_DB_USER=$5
 WP_DB_NAME=$6
+APACHE_SITE_NAME=$7
 
 cp ./wordpress/wp-config.php.template ./wordpress/wp-config.php
 
@@ -18,5 +19,6 @@ sed -i "s/{{WP_CONFIG_KEYS}}/$(echo $WP_CONFIG_KEYS | sed -e 's/\\/\\\\/g; s/\//
 sed -i "s/{{WP_SITE_URL}}/$WP_SITE_URL/" ./wordpress/wp-config.php
 sed -i "s/{{WP_DB_USER}}/$WP_DB_USER/" ./wordpress/wp-config.php
 sed -i "s/{{WP_DB_NAME}}/$WP_DB_NAME/" ./wordpress/wp-config.php
+sed -i "s/{{APACHE_SITE_NAME}}/$APACHE_SITE_NAME/" ./wordpress/wp-config.php
 
 echo '[WP_CONFIG] Config transformation succeeded'
