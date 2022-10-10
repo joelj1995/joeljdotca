dfb3843f1ce9d0f067eb34e7eed21aa1d29a7028
Joel Johnston <colter13@gmail.com>
2022-10-10 16:37:42 -0600

    Switch over to contentful

--- blog-app/src/environments/environment.prod.ts
+++ blog-app/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   wpBaseUrl: '',
   features: {
     SUBSCRIBE: true,
-    CONTENTFUL: false
+    CONTENTFUL: true
   },
   contentfulSpace: 'jdcejwtspy00',
   contentfulAccessToken: '5c8wf63ztfgSk7xIHTMOz2pTdzCq_WRPJ-X1iwhOGAA'
--- blog-app/src/environments/environment.ts
+++ blog-app/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   wpBaseUrl: 'https://wwwtest.joelj.ca',
   features: {
     SUBSCRIBE: true,
-    CONTENTFUL: false
+    CONTENTFUL: true
   },
   contentfulSpace: 'jdcejwtspy00',
   contentfulAccessToken: '5c8wf63ztfgSk7xIHTMOz2pTdzCq_WRPJ-X1iwhOGAA'
