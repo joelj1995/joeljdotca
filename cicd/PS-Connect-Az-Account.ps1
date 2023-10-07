314958c5cddb461544d1f85607dc7035e1d84c39
Joel Johnston <me@joelj.ca>
2023-10-07 11:51:19 -0600

    Step to get secrets

--- cicd/PS-Connect-Az-Account.ps1
+++ cicd/PS-Connect-Az-Account.ps1
@@ -1,3 +1,4 @@
+Write-Host 'IN PS-Connect-AZ-Account.ps1'
 $SecureStringPwd = $Env:JJ_AZ_SECRET | ConvertTo-SecureString -AsPlainText -Force
 $AzPsCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Env:JJ_AZ_APP_ID, $SecureStringPwd
 Connect-AzAccount -ServicePrincipal -Credential $AzPsCredential -Tenant $Env:JJ_AZ_TENANT_ID
